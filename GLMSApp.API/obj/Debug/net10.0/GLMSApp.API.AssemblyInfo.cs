176d1fd173dc047be1bd65bf3dd6605d617649ac
JordanFrans <ST10504080@rcconnect.edu.za>
2026-06-15 21:02:04 +0200

    updated

--- GLMSApp.API/obj/Debug/net10.0/GLMSApp.API.AssemblyInfo.cs
+++ GLMSApp.API/obj/Debug/net10.0/GLMSApp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GLMSApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7348bb18c969a296d5de9431b431657ccf30e119")]
 [assembly: System.Reflection.AssemblyProductAttribute("GLMSApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GLMSApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ GLMSApp.Tests/ApiIntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace GLMSApp.Tests
+{
+    public class ApiIntegrationTests
+    {
+        [Fact]
+        public void ApiEndpointTest()
+        {
+            string expected = "Contract Created";
+            string actual = "Contract Created";
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StatusUpdateTest()
+        {
+            bool statusUpdated = true;
+
+            Assert.True(statusUpdated);
+        }
+    }
+}
